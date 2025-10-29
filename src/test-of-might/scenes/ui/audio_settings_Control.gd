e86492b7114067a9f635c2ea2a4db478bd046d31
Dawid <dawidkuta77@gmail.com>
2025-10-29 23:20:41 +0100

    Implementacja Audio
    Dodałem tam proste menu bez jakiś assetów ani nic jest ono w scenes/ui/Audio...tscn można odpalić jest ustawiona muzyczka na start i zobaczyć czy działa (za nią odpowiada suwak Muzyka)
    
    - W folder Assety dodałem też folder Audio z folderami Music i SFX

---
+++ src/test-of-might/scenes/ui/audio_settings_Control.gd
@@ -0,0 +1,20 @@
+extends Control
+
+@onready var music_bus_id: int = AudioServer.get_bus_index("Music")
+@onready var sfx_bus_id: int = AudioServer.get_bus_index("SFX")
+
+@onready var music_slider: HSlider = $MarginContainer/VBoxContainer/GridContainer/Music_HSlider
+@onready var sfx_slider: HSlider = $MarginContainer/VBoxContainer/GridContainer/SFX_HSlider
+
+func _ready() -> void:
+	music_slider.value = db_to_linear(AudioServer.get_bus_volume_db(music_bus_id))
+	sfx_slider.value = db_to_linear(AudioServer.get_bus_volume_db(sfx_bus_id))
+
+func _on_music_h_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(music_bus_id, linear_to_db(value))
+	AudioServer.set_bus_mute(music_bus_id, value < 0.05)
+
+
+func _on_sfx_h_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(sfx_bus_id, linear_to_db(value))
+	AudioServer.set_bus_mute(sfx_bus_id, value < 0.05)
