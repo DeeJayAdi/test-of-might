7cb9621f50ca4d742288fec92638f9ede65c20cd
suinama <opklmjhurfgujhhyfdcx@gmail.com>
2025-11-09 14:30:45 +0100

    Added a working inventory

---
+++ src/test-of-might/scripts/player/inventory/equipment_slot.gd
@@ -0,0 +1,59 @@
+extends Panel
+
+@onready var icon: TextureRect = $icon
+@export var item: ItemData
+@export var slot_type: String = ""  
+
+signal item_equipped(item: ItemData)
+signal item_unequipped(item: ItemData)
+
+func _ready() -> void:
+	update_ui()
+
+func update_ui() -> void:
+	if not item:
+		icon.texture = null
+		tooltip_text = ""
+	else:
+		icon.texture = item.icon
+		tooltip_text = item.item_name
+
+func _get_drag_data(_at_position: Vector2) -> Variant:
+	if not item:
+		return
+	var preview = duplicate()
+	var c = Control.new()
+	c.add_child(preview)
+	preview.position -= Vector2(25, 25)
+	preview.self_modulate = Color.TRANSPARENT
+	c.modulate = Color(c.modulate, 0.5)
+	set_drag_preview(c)
+	icon.hide()
+	return self
+
+func _can_drop_data(_at_position: Vector2, data: Variant) -> bool:
+	if not data or not data.item:
+		return false
+	return data.item.type == slot_type
+
+func _drop_data(_at_position: Vector2, data: Variant) -> void:
+	if not _can_drop_data(_at_position, data):
+		DisplayServer.cursor_set_shape(DisplayServer.CURSOR_FORBIDDEN)
+		return
+	
+	# Swap items
+	var tmp = item
+	item = data.item
+	data.item = tmp
+	
+	icon.show()
+	data.icon.show()
+	update_ui()
+	data.update_ui()
+
+	# Emit signals to update stats
+	if item:
+		emit_signal("item_equipped", item)
+	if tmp:
+		emit_signal("item_unequipped", tmp)
+		
---
+++ src/test-of-might/scripts/player/inventory/item_data.gd
@@ -0,0 +1,14 @@
+extends Resource
+class_name ItemData
+
+@export var item_name: String = ""
+@export var rarity: String = ""
+@export var type: String = ""
+@export var subtype: String = ""
+@export var icon: Texture2D
+
+@export var damage: int
+@export var defense: int
+@export var attack_range: float
+@export var attack_speed: float
+@export var effect: String = ""
