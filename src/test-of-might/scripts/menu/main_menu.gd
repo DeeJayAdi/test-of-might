95e93cf3c4cc8f6b84a5c21f3396df18f7c2144b
Dawid <dawidkuta77@gmail.com>
2025-11-14 16:35:33 +0100

    Poprawki do zapisu
    Poprawiłem menu pauzy
    dodałem dwa nowe menu do ładowania savów
    i tworzenia nowej gry
    oraz dodałem możliwość tworzenia 4 osobnych savów

--- src/test-of-might/scripts/menu/main_menu.gd
+++ src/test-of-might/scripts/menu/main_menu.gd
@@ -4,14 +4,13 @@ extends Control
 func _on_new_game_pressed() -> void:
 	var temp: String = get_tree().current_scene.scene_file_path
 	PreviousScene.previous_scene_path = temp
-	get_tree().change_scene_to_file("res://scenes/map_menu/map_menu.tscn")
+	get_tree().change_scene_to_file("res://scenes/menu/NewGameSlot.tscn")
 
 func _on_load_pressed() -> void:
 	var temp: String = get_tree().current_scene.scene_file_path
 	PreviousScene.previous_scene_path = temp
-	get_tree().change_scene_to_file("res://maps/cave/cave.tscn")
-	if is_instance_valid(PersistentMusic):
-		PersistentMusic.queue_free()
+
+	get_tree().change_scene_to_file("res://scenes/menu/Load_Save_Menu.tscn")
 
 
 
