cccf69a720f57888d34739f551ee94eb1e012d3f
Dawid <dawidkuta77@gmail.com>
2025-11-19 19:19:50 +0100

    Mechanika Mikstur
    Dodałem Mechanikę mikstur jedynie póki co mikstur do leczenia:
    -te co od razu leczą
    -healing over time
    -mikstura ,,Vital" która łączy te dwie cechy co wyżej
    
    Dodałem też label dodatkowy do ekwipunku żeby mieć ilość przedmitów i jest to zaimplementowane już, aby to sprawdzić trzeba przenieść miksturkę w inne okienko i dopiero pojawi się ilość jej
    
    Nie dodawałem więcej mikstur bo nie mamy żadnych statystyk aby buffować naszą postać typu większy def albo dmg itd.(oraz nie wiem czy chcemy takie coś)

--- src/test-of-might/scripts/player/inventory/item_slot.gd
+++ src/test-of-might/scripts/player/inventory/item_slot.gd
@@ -2,19 +2,33 @@ extends Panel
 
 @onready var icon: TextureRect = $icon
 @export var item: ItemData
+@onready var quantity_label: Label = $QuantityLabel
+var quantity: int = 0
+var player_node: CharacterBody2D = null
 
 
 
 func _read() -> void:
+	if item:
+		quantity = item.default_quantity
 	update_ui()
 
 func update_ui() -> void:
 	if not item:
 		icon.texture = null
+		if quantity_label: quantity_label.visible = false
+		quantity = 0 
 		return
 		
+	if quantity <= 0:
+		quantity = max(1, item.default_quantity)
 	icon.texture = item.icon
 	tooltip_text = item.item_name
+	if quantity > 1:
+		quantity_label.text = str(quantity)
+		quantity_label.visible = true
+	else:
+		quantity_label.visible = false 
 
 func _get_drag_data(_at_position: Vector2) -> Variant:
 	if not item:
@@ -38,8 +52,32 @@ func _drop_data(_at_position: Vector2, data: Variant) -> void:
 	var tmp = item
 	item = data.item
 	data.item = tmp
+	var tmp_quantity = quantity
+	quantity = data.quantity
+	data.quantity = tmp_quantity
 	icon.show()
 	data.icon.show()
 	update_ui()
 	data.update_ui()
 	
+func set_player(player):
+	player_node = player
+
+func use_item():
+	if not item:
+		return
+	if not player_node:
+		return
+	if item.type == "potion":
+		print("Using potion:", item.item_name)
+		if item.heal_instant > 0:
+			player_node.heal(item.heal_instant)
+		if item.heal_per_second > 0 and item.heal_duration > 0:
+			player_node.heal_over_time(item.heal_per_second, item.heal_duration)
+		quantity -= 1
+		if quantity <= 0:
+			item = null		
+		update_ui()
+func _gui_input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
+		use_item()
